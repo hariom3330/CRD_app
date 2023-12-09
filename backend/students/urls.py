00d5f8e609617994d236700e7d109d8d93c142ab
hariom3330 <hariomparmar330@gmail.com>
2023-12-09 13:37:12 +0530

    First Commit

---
+++ backend/students/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import StudentView
+from . import views
+
+urlpatterns = [
+    path("students/", StudentView.as_view()),
+    path("students/<int:pk>/", StudentView.as_view()),
+    path('',views.index,name='index')
+]
